5221a81e05092e6e8d3dceb47ccbd96e2296ad53
nieyo <jonasbucher95@gmail.com>
2025-02-24 15:00:39 +0100

    Initial commit

---
+++ src/main/java/org/example/nfjavarecap04/NfJavaRecap04Application.java
@@ -0,0 +1,13 @@
+package org.example.nfjavarecap04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfJavaRecap04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfJavaRecap04Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/nfjavarecap04/NfJavaRecap04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nfjavarecap04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfJavaRecap04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
